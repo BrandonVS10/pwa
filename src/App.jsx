aee6a986a7f74bd85e73cf7d40bee779df6dec4b
BrandonVS10 <brandon.vazquez.22e@utzmg.edu.mx>
2025-03-27 11:09:29 -0600

    nuevoso

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes,Route } from "react-router-dom";
 import Home from "./components/Home";
-import Login from "./components/Register";
-import Register from "./components/Login";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 export default function App() {
   return (
     <Routes>
       
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Register />} />
-        <Route path="/registro" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
     </Routes>
   );
 }

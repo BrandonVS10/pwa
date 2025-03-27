d6a3c789e827d9a451dfb42f69c8806d4f88082f
BrandonVS10 <brandon.vazquez.22e@utzmg.edu.mx>
2025-03-27 09:21:32 -0600

    nuevo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, // Usa el puerto asignado por Render
+    allowedHosts: ['https://pwa-m875.onrender.com'],
+  },
+});
